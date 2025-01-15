d88be0a95b0deb1808e4f3bc311a0cab3c4967a7
hyhflower <heyunhua5_20@163.com>
2025-01-15 16:57:08 +0800

    V1.3

---
+++ build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/DependInfo.cmake
@@ -0,0 +1,81 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/camera_emu/dji_media_file_manage/dji_media_file_core.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/camera_emu/dji_media_file_manage/dji_media_file_core.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/camera_emu/dji_media_file_manage/dji_media_file_jpg.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/camera_emu/dji_media_file_manage/dji_media_file_jpg.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/camera_emu/dji_media_file_manage/dji_media_file_mp4.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/camera_emu/dji_media_file_manage/dji_media_file_mp4.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/camera_emu/test_payload_cam_emu_base.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/camera_emu/test_payload_cam_emu_base.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/camera_emu/test_payload_cam_emu_media.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/camera_emu/test_payload_cam_emu_media.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/camera_manager/test_camera_manager.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/camera_manager/test_camera_manager.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/data_transmission/test_data_transmission.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/data_transmission/test_data_transmission.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/fc_subscription/test_fc_subscription.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/fc_subscription/test_fc_subscription.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/flight_control/test_flight_control.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/flight_control/test_flight_control.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/gimbal_emu/test_payload_gimbal_emu.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/gimbal_emu/test_payload_gimbal_emu.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/gimbal_manager/test_gimbal_manager.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/gimbal_manager/test_gimbal_manager.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/hms/test_hms.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/hms/test_hms.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/interest_point/test_interest_point.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/interest_point/test_interest_point.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/liveview/test_liveview.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/liveview/test_liveview.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/mop_channel/test_mop_channel.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/mop_channel/test_mop_channel.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/payload_collaboration/test_payload_collaboration.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/payload_collaboration/test_payload_collaboration.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/perception/test_perception.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/perception/test_perception.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/positioning/test_positioning.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/positioning/test_positioning.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/power_management/test_power_management.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/power_management/test_power_management.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/time_sync/test_time_sync.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/time_sync/test_time_sync.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/upgrade/test_upgrade.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/upgrade/test_upgrade.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/upgrade/test_upgrade_common_file_transfer.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/upgrade/test_upgrade_common_file_transfer.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/upgrade/test_upgrade_platform_opt.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/upgrade/test_upgrade_platform_opt.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/cJSON.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/cJSON.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/dji_config_manager.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/dji_config_manager.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/util_buffer.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/util_buffer.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/util_file.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/util_file.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/util_link_list.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/util_link_list.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/util_md5.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/util_md5.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/util_misc.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/util_misc.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/utils/util_time.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/utils/util_time.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/waypoint_v2/test_waypoint_v2.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/waypoint_v2/test_waypoint_v2.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/waypoint_v3/test_waypoint_v3.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/waypoint_v3/test_waypoint_v3.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/widget/file_binary_array_list_en.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/widget/file_binary_array_list_en.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/widget/test_widget.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/widget/test_widget.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/widget/test_widget_speaker.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/widget/test_widget_speaker.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/widget_interaction_test/file_binary_array_list_en.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/widget_interaction_test/file_binary_array_list_en.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/widget_interaction_test/test_widget_interaction.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/widget_interaction_test/test_widget_interaction.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/module_sample/xport/test_payload_xport.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/__/__/module_sample/xport/test_payload_xport.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/common/monitor/sys_monitor.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/common/monitor/sys_monitor.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/common/osal/osal.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/common/osal/osal.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/common/osal/osal_fs.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/common/osal/osal_fs.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/common/osal/osal_socket.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/common/osal/osal_socket.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/common/upgrade_platform_opt/upgrade_platform_opt_linux.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/__/common/upgrade_platform_opt/upgrade_platform_opt_linux.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/manifold2/application/fintek/demo_gpio_int.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/application/fintek/demo_gpio_int.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/manifold2/hal/hal_network.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/hal/hal_network.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/manifold2/hal/hal_uart.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/hal/hal_uart.c.o"
+  "/home/hhit/Payload-SDK/samples/sample_c/platform/linux/manifold2/hal/hal_usb_bulk.c" "/home/hhit/Payload-SDK/build/samples/sample_c/platform/linux/manifold2/CMakeFiles/dji_sdk_demo_linux.dir/hal/hal_usb_bulk.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "LIBUSB_INSTALLED"
+  "OPUS_INSTALLED"
+  "PLATFORM_ARCH_x86_64=1"
+  "SYSTEM_ARCH_LINUX"
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../samples/sample_c/platform/linux/manifold2/../../../module_sample"
+  "../samples/sample_c/platform/linux/manifold2/../common"
+  "../samples/sample_c/platform/linux/manifold2/../manifold2/application"
+  "../samples/sample_c/platform/linux/manifold2/../manifold2/application/fintek"
+  "../samples/sample_c/platform/linux/manifold2/../../../../../psdk_lib/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
